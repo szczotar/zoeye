f24f3cc4a5e89d696083d19e43154a428506715b
unknown <szczotar@gmail.com>
2025-04-13 22:38:22 +0200

    login

--- store/views.py
+++ store/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .models import Product
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+def Logout_user(request):
+     pass
+
