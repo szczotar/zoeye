66aba0d0ab26c498c704cc82ecba45b18798a0ee
unknown <szczotar@gmail.com>
2025-05-17 20:46:15 +0200

    change db con

--- practice/settings.py
+++ practice/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER':'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': 'yamabiko.proxy.rlwy.net',
         'PORT': 'x`'
     }
